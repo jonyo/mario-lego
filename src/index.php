57fa4c0a0121f62cb508fde4432283e9b363479d
Jonathan Foote <jf@jonathanfoote.com>
2020-10-25 10:16:15 -0500

    Starting poing -- experimenting

---
+++ src/index.php
@@ -0,0 +1,27 @@
+<?php
+$codes = [];
+
+$codes['start'] = []
+
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <!-- My Simple Website -->
+    <title>Jonathan Foote's Site</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <line x1="0" y1="80" x2="100" y2="20" stroke="black" />
+    </svg>
+    <article class="barcode">
+        <span class="barcode__line"></span>
+        <span class="barcode__line"></span>
+        <span class="barcode__line"></span>
+        <span class="barcode__line"></span>
+        <span class="barcode__line"></span>
+    </article>
+</body>
