7d45dff37d94476860f644ead4ac1ed550053bfe
Jonathan Foote <jf@jonathanfoote.com>
2020-10-26 10:30:07 -0500

    Adding more codes

--- src/codes.php
+++ src/codes.php
@@ -1,40 +1,39 @@
 <?php
-
-// blind bag patterns - shared between them
+// blind bag patterns - shared between enemies
 $blindBags = [
     'a' => ['dark-green', 'red', 'blue', 'pink', 'yellow'],
     'b' => ['dark-green', 'red', 'purple', 'yellow', 'pink'],
     'c' => ['dark-green', 'red', 'pink', 'purple', 'yellow'],
 ];
 
-$codes = [
+return [
     // starter pack
     'start' => [
-        'pattern' => ['blue', 'light-green', 'red', 'dark-green', 'purple'],
+        'pattern' => ['dark-green', 'red', 'light-green', 'blue', 'purple'],
         'title' => 'start',
     ],
     'goal' => [
-        'pattern' => ['red', 'light-green', 'blue', 'yellow', 'dark-green'],
+        'pattern' => ['dark-green', 'red', 'light-green', 'blue', 'yellow'],
         'title' => 'goal',
     ],
     'goomba' => [
-        'pattern' => ['red', 'blue', 'pink', 'yellow', 'dark-green'],
+        'pattern' => ['dark-green', 'red', 'blue', 'pink', 'yellow'],
         'title' => 'goomba',
     ],
     'question-block' => [
-        'pattern' => ['red', 'pink', 'yellow', 'blue', 'dark-green'],
+        'pattern' => ['dark-green', 'red', 'pink', 'yellow', 'blue'],
         'title' => '? block',
     ],
     'cloud-platform' => [
-        'pattern' => ['red', 'pink', 'purple', 'blue', 'dark-green'],
+        'pattern' => ['dark-green', 'red', 'pink', 'purple', 'blue'],
         'title' => '☁️ platform',
     ],
     'rotation-platform' => [
-        'pattern' => ['red', 'blue', 'purple', 'pink', 'dark-green'],
+        'pattern' => ['dark-green', 'red', 'blue', 'purple', 'pink'],
         'title' => '🔄 platform',
     ],
     'bowser-jr' => [
-        'pattern' => ['red', 'teal', 'blue', 'yellow', 'dark-green'],
+        'pattern' => ['dark-green', 'red', 'teal', 'blue', 'yellow'],
         'title' => 'bowser jr',
     ],
 
@@ -61,7 +60,7 @@ $codes = [
         'pattern' => ['dark-green', 'red', 'blue', 'pink', 'teal'],
         'title' => 'whomp',
     ],
-    'brigde-slide' => [
+    'bridge-slide' => [
         'pattern' => ['dark-green', 'red', 'pink', 'yellow', 'teal'],
         'title' => 'bridge slide',
     ],
@@ -160,10 +159,12 @@ $codes = [
     */
 
     // Found... https://drive.google.com/drive/folders/1T9pvU4igusXtqS5yFyxmRKnSD9FYbbsf
+    /*
     '' => [
-        'pattern' => [],
+        'pattern' => ['dark-green', 'red',],
         'title' => '',
     ],
+    */
     'king-boo' => [
         'pattern' => ['dark-green', 'red', 'purple', 'yellow', 'blue'],
         'title' => 'king 👻',
@@ -184,6 +185,94 @@ $codes = [
         'pattern' => ['dark-green', 'red', 'purple', 'pink', 'blue'],
         'title' => 'piranha plant',
     ],
+    'boo' => [
+        'pattern' => ['dark-green', 'red', 'purple', 'pink', 'light-green'],
+        'title' => 'boo',
+    ],
+    'pokey' => [
+        'pattern' => ['dark-green', 'red', 'yellow', 'purple', 'light-green'],
+        'title' => 'pokey',
+    ],
+    'yoshi-speech' => [
+        'pattern' => ['dark-green', 'red', 'yellow', 'blue', 'purple'],
+        'title' => 'yoshi',
+    ],
+    // 71369
+    'cyclical-arrows' => [
+        'pattern' => ['dark-green', 'red', 'yellow', 'teal', 'purple'],
+        'title' => 'cyclical arrows',
+    ],
+    // 71362
+    'koopa-troopa' => [
+        'pattern' => ['dark-green', 'red', 'yellow', 'teal', 'light-green'],
+        'title' => 'koopa troopa',
+    ],
+    // 71362
+    'coin1' => [
+        'pattern' => ['dark-green', 'red', 'yellow', 'teal', 'pink'],
+        'title' => 'coin',
+    ],
+    'dry-bones' => [
+        'pattern' => ['dark-green', 'red', 'blue', 'purple', 'yellow'],
+        'title' => 'dry bones',
+    ],
+    // 71360
+    'cyclical-arrows2' => [
+        'pattern' => ['dark-green', 'red', 'blue', 'purple', 'light-green'],
+        'title' => 'cyclical arrows',
+    ],
+    'mario-melody' => [
+        'pattern' => ['dark-green', 'red', 'blue', 'yellow', 'pink'],
+        'title' => 'mario melody',
+    ],
+    'hammer' => [
+        'pattern' => ['dark-green', 'red', 'blue', 'light-green', 'pink'],
+        'title' => 'hammer',
+    ],
+    // 71366
+    'cyclical-arrows4' => [
+        'pattern' => ['dark-green', 'red', 'blue', 'light-green', 'teal'],
+        'title' => 'cyclical arrows',
+    ],
+    'shy-guy' => [
+        'pattern' => ['dark-green', 'red', 'blue', 'pink', 'purple'],
+        'title' => 'shy guy',
+    ],
+    // 81369
+    'cyclical-arrows5' => [
+        'pattern' => ['dark-green', 'red', 'blue', 'teal', 'yellow'],
+        'title' => 'cyclical arrows',
+    ],
+    // 71377
+    'coin2' => [
+        'pattern' => ['dark-green', 'red', 'light-green', 'pink', 'teal'],
+        'title' => 'coin',
+    ],
+    // 71376
+    'bidirectional-arrows' => [
+        'pattern' => ['dark-green', 'red', 'pink', 'purple', 'teal'],
+        'title' => 'arrows',
+    ],
+    // 71365
+    'bidirectional-arrows2' => [
+        'pattern' => ['dark-green', 'red', 'pink', 'light-green', 'blue'],
+        'title' => 'arrows',
+    ],
+    'toad-speech' => [
+        'pattern' => ['dark-green', 'red', 'pink', 'light-green', 'teal'],
+        'title' => 'toad speech',
+    ],
+    // 71362 ?? same as other
+    'koopa-troopa2' => [
+        'pattern' => ['dark-green', 'red', 'pink', 'teal', 'purple'],
+        'title' => 'koopa troopa',
+    ],
+    'lava' => [
+        'pattern' => ['dark-green', 'red', 'pink', 'teal', 'yellow'],
+        'title' => 'lava',
+    ],
+    'thwamp' => [
+        'pattern' => ['dark-green', 'red', 'pink', 'teal', 'light-green'],
+        'title' => 'thwamp',
+    ],
 ];
-
-return $codes;
