f75f058258fb87339722c921e34d8f2c6e95d2e0
Jonathan Foote <jf@jonathanfoote.com>
2020-10-27 13:50:30 -0500

    Remove the old commented-colors that did not work

--- webroot/barcode.php
+++ webroot/barcode.php
@@ -16,16 +16,16 @@ header('Vary: Accept-Encoding');
 
 /**
  * Notes:
- * 
+ *
  * Styling:  20.1mm width for correct barcode size
- * 
+ *
  * Uses pattern:
  * | color | space | color | space | color | space | color | space | color | space |
- * 
+ *
  * Where each color is .2mm wide and each space is .2mm wide.
- * 
+ *
  * Barcodes are 5 colors that repeat.  This SVG repeats the pattern 10 times, making a 20x20cm square.
- * 
+ *
  * Viewbox starts at -1 to avoid issues when the image is repeated, and is why it needs 20.1mm width to account for
  * stroke width.
  */
@@ -34,14 +34,14 @@ header('Vary: Accept-Encoding');
 <svg viewBox="-1 0 200 200" xmlns="http://www.w3.org/2000/svg">
     <style>
         :root {
-            --red: #b22222;/* #c9191a; */
-            --yellow: #fad700; /* #fadd13; */
-            --light-green: #9acd33; /* #b9d041; */
-            --dark-green: #2e8b57; /* #1e9046 */
-            --teal: #26b2aa; /* #23a5a1; */
-            --blue: #4169e1; /* #246bb3; */
-            --purple: #9370db; /* #a57ab4; */
-            --pink: #da70d6; /* #d562a1; */
+            --red: #b22222;
+            --yellow: #fad700;
+            --light-green: #9acd33;
+            --dark-green: #2e8b57;
+            --teal: #26b2aa;
+            --blue: #4169e1;
+            --purple: #9370db;
+            --pink: #da70d6;
         }
     </style>
     <defs>
