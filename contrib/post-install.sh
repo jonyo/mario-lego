673e0d6d107a0bf876ed104f69e4d6da394a60ae
Jonathan Foote <jf@jonathanfoote.com>
2020-10-30 22:12:22 -0500

    Use sourcemap for compiled CSS to make it easier to troubleshoot

--- contrib/post-install.sh
+++ contrib/post-install.sh
@@ -4,5 +4,5 @@
 [ -d "webroot/dist" ] || mkdir webroot/dist
 
 # Compile the sass css
-./vendor/bin/pscss --style=compressed src/sass/index.scss > webroot/dist/index.css
-./vendor/bin/pscss --style=compressed src/sass/generate.scss > webroot/dist/generate.css
+./vendor/bin/pscss --style=compressed --sourcemap src/sass/index.scss > webroot/dist/index.css
+./vendor/bin/pscss --style=compressed --sourcemap src/sass/generate.scss > webroot/dist/generate.css
